7fbe74ea56028ac1afa758864808a0bdf8b1cae6
814725243016-collab <814725243016@trp.srmtrichy.edu.in>
2026-02-16 14:38:29 +0530

    Update program.py

--- program.py
+++ program.py
@@ -1,4 +1,12 @@
 a=10
-b=14
-c=40
-print(a,b,c)
+b=5
+sum=a+b
+product=a*b
+greater=a>b
+lesser=a<b
+logical=a>b & a<b
+print("sum is:",sum)
+print("product is:",product)
+print( Is a greater than b:",greaster)
+print("Is a lesser tham b:",lesser)
+print("logical and result is :",logical)
